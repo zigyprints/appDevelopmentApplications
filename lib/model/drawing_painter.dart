53d9f3c23c1b7aa0f5c4ee73d4bb88d74b86b2b3
Jonaxx8 <20r21a0543@mlrinstitutions.ac.in>
2023-09-08 17:31:00 +0530

    user is able to draw on the image selected

--- lib/model/drawing_painter.dart
+++ lib/model/drawing_painter.dart
@@ -4,11 +4,30 @@ class DrawingPainter extends CustomPainter {
   final List<Offset?> points; // Use nullable Offset here
   final Color color;
   final double strokeWidth;
+  final ImageProvider<Object>? backgroundImage;
 
-  DrawingPainter(this.points, this.color, this.strokeWidth);
+  DrawingPainter(this.points, this.color, this.strokeWidth, {this.backgroundImage});
 
   @override
   void paint(Canvas canvas, Size size) {
+    if (backgroundImage != null) {
+      final Rect rect = Offset.zero & size;
+      final Paint paint = Paint();
+      paint.color = Colors.white; // You can set a background color here if needed
+      canvas.drawRect(rect, paint);
+
+      // Load the background image and draw it
+      final image = backgroundImage!.resolve(ImageConfiguration.empty);
+      image.addListener(ImageStreamListener((ImageInfo info, bool _) {
+        canvas.drawImageRect(
+          info.image,
+          Rect.fromLTRB(0, 0, info.image.width.toDouble(), info.image.height.toDouble()),
+          rect,
+          Paint(),
+        );
+      }));
+    }
+
     final Paint paint = Paint()
       ..color = color
       ..strokeCap = StrokeCap.round
--- lib/screens/drawing_screen.dart
+++ lib/screens/drawing_screen.dart
@@ -24,176 +24,181 @@ class _DrawingScreenState extends State<DrawingScreen> {
   String? imagePath; // Store the path of the selected image
 
   @override
-  Widget build(BuildContext context) {
-    final drawingModel = Provider.of<DrawingScreenModel>(context);
+Widget build(BuildContext context) {
+  final drawingModel = Provider.of<DrawingScreenModel>(context);
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Drawing Screen'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.undo),
-            onPressed: () {
-              drawingModel.undo(); // Call the undo method from your model
-            },
-          ),
-          IconButton(
-            icon: Icon(Icons.redo),
-            onPressed: () {
-              drawingModel.redo(); // Call the redo method from your model
-            },
-          ),
-        ],
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: GestureDetector(
-              onPanUpdate: (details) {
-                RenderBox renderBox = context.findRenderObject() as RenderBox;
-                final localPosition =
-                    renderBox.globalToLocal(details.globalPosition);
-                if (_selectedIndex == 0) {
-                  // Check if Pencil icon is selected
-                  if (!isEraserMode) {
-                    points.add(localPosition);
-                  } else {
-                    // In eraser mode, remove points that are close to the touch point.
-                    points.removeWhere((point) =>
-                        point != null &&
-                        (point - localPosition).distance < strokeWidth);
-                  }
-                  drawingModel.notifyListeners();
-                }
-              },
-              onPanEnd: (details) {
-                // When the user lifts their finger, add a null point to separate strokes.
-                points.add(null);
-                undonePoints.clear(); // Clear undone points when new drawing action is performed
-              },
-              child: CustomPaint(
-                painter: DrawingPainter(points, currentColor, strokeWidth),
-                size: Size.infinite,
-                child: imagePath != null
-                    ? Image.file(
-                        File(imagePath!),
-                        fit: BoxFit.cover,
-                      )
-                    : SizedBox(),
-              ),
-            ),
-          ),
-        ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedIndex,
-        onTap: (index) {
-          setState(() {
-            _selectedIndex = index;
-            // Handle bottom navigation item selection here
-            if (index == 1) {
-              // Eraser icon selected
-              setState(() {
-                isEraserMode = true;
-                _selectedIndex = 0; // Set the navbar index to Pencil
-                points.clear(); // Clear all drawing points
-                undonePoints.clear(); // Clear undone points
-              });
-            } else if (index == 0) {
-              // Pencil icon selected
-              setState(() {
-                isEraserMode = false;
-                currentColor = selectedColor; // Set current color to selected color
-              });
-            } else if (index == 2) {
-              // Color picker icon selected
-              showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return AlertDialog(
-                    title: Text('Pick a color'),
-                    content: SingleChildScrollView(
-                      child: ColorPicker(
-                        pickerColor: currentColor,
-                        onColorChanged: (color) {
-                          setState(() {
-                            selectedColor = color;
-                            isEraserMode =
-                                false; // Exit eraser mode when picking a color.
-                            _selectedIndex = 0; // Set the navbar index to Pencil
-                            points.clear(); // Clear all drawing points
-                            undonePoints.clear(); // Clear undone points
-                          });
-                        },
-                        showLabel: true,
-                        pickerAreaHeightPercent: 0.8,
-                      ),
-                    ),
-                    actions: [
-                      TextButton(
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                        child: Text('OK'),
-                      ),
-                    ],
-                  );
-                },
-              );
-            } else if (index == 3) {
-              // Gallery icon selected
-              _pickImageFromGallery(); // Function to pick image from gallery
+  return Scaffold(
+    appBar: AppBar(
+      title: Text('Drawing Screen'),
+      actions: [
+        IconButton(
+          icon: Icon(Icons.undo),
+          onPressed: () {
+            drawingModel.undo(); // Call the undo method from your model
+          },
+        ),
+        IconButton(
+          icon: Icon(Icons.redo),
+          onPressed: () {
+            drawingModel.redo(); // Call the redo method from your model
+          },
+        ),
+      ],
+    ),
+    body: Column(
+  children: [
+    Expanded(
+      child: GestureDetector(
+        onPanUpdate: (details) {
+          RenderBox renderBox = context.findRenderObject() as RenderBox;
+          final localPosition = renderBox.globalToLocal(details.globalPosition);
+          if (_selectedIndex == 0) {
+            // Check if Pencil icon is selected
+            if (!isEraserMode) {
+              points.add(localPosition);
+            } else {
+              // In eraser mode, remove points that are close to the touch point.
+              points.removeWhere((point) =>
+                  point != null &&
+                  (point - localPosition).distance < strokeWidth);
             }
-          });
+            drawingModel.notifyListeners();
+          }
         },
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.create,
-              color: Colors.black,
-              size: 28,
-            ), // Pencil icon
-            label: '', // Set label to an empty string
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.check_box_outline_blank_outlined,
-              color: Colors.black,
-              size: 28,
-            ), // Eraser icon
-            label: '', // Set label to an empty string
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.color_lens,
-              color: Colors.black,
-              size: 28,
-            ), // Color picker icon
-            label: '', // Set label to an empty string
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.photo_library,
-              color: Colors.black,
-              size: 28,
-            ), // Gallery icon
-            label: '', // Set label to an empty string
-          ),
-        ],
+        onPanEnd: (details) {
+          // When the user lifts their finger, add a null point to separate strokes.
+          points.add(null);
+          undonePoints.clear(); // Clear undone points when a new drawing action is performed
+        },
+        child: Stack(
+          children: [
+            if (imagePath != null)
+              Positioned.fill(
+                child: Image.file(
+                  File(imagePath!),
+                  fit: BoxFit.contain,
+                ),
+              ),
+            CustomPaint(
+              painter: DrawingPainter(points, currentColor, strokeWidth),
+              size: Size.infinite,
+            ),
+          ],
+        ),
       ),
-    );
-  }
+    ),
+  ],
+),
 
-  // Function to pick an image from the gallery
-// Function to pick an image from the gallery
-  // Function to pick an image from the gallery
-  _pickImageFromGallery() async {
-    final pickedFile = await ImagePicker().pickImage(source: ImageSource.gallery);
-    if (pickedFile != null) {
-      setState(() {
-        imagePath = pickedFile.path;
-      });
-    }
-  }
-
+    bottomNavigationBar: BottomNavigationBar(
+      currentIndex: _selectedIndex,
+      onTap: (index) {
+        setState(() {
+          _selectedIndex = index;
+          // Handle bottom navigation item selection here
+          if (index == 1) {
+            // Eraser icon selected
+            setState(() {
+              isEraserMode = true;
+              _selectedIndex = 0; // Set the navbar index to Pencil
+              points.clear(); // Clear all drawing points
+              undonePoints.clear(); // Clear undone points
+            });
+          } else if (index == 0) {
+            // Pencil icon selected
+            setState(() {
+              isEraserMode = false;
+              currentColor = selectedColor; // Set current color to selected color
+            });
+          } else if (index == 2) {
+            // Color picker icon selected
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: Text('Pick a color'),
+                  content: SingleChildScrollView(
+                    child: ColorPicker(
+                      pickerColor: currentColor,
+                      onColorChanged: (color) {
+                        setState(() {
+                          selectedColor = color;
+                          isEraserMode =
+                              false; // Exit eraser mode when picking a color.
+                          _selectedIndex = 0; // Set the navbar index to Pencil
+                          points.clear(); // Clear all drawing points
+                          undonePoints.clear(); // Clear undone points
+                        });
+                      },
+                      showLabel: true,
+                      pickerAreaHeightPercent: 0.8,
+                    ),
+                  ),
+                  actions: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Text('OK'),
+                    ),
+                  ],
+                );
+              },
+            );
+          } else if (index == 3) {
+            // Gallery icon selected
+            _pickImageFromGallery(); // Function to pick an image from the gallery
+          }
+        });
+      },
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.create,
+            color: Colors.black,
+            size: 28,
+          ),
+          label: '',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.check_box_outline_blank_outlined,
+            color: Colors.black,
+            size: 28,
+          ),
+          label: '',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.color_lens,
+            color: Colors.black,
+            size: 28,
+          ),
+          label: '',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.photo_library,
+            color: Colors.black,
+            size: 28,
+          ),
+          label: '',
+        ),
+      ],
+    ),
+  );
 }
+
+// Function to pick an image from the gallery
+_pickImageFromGallery() async {
+  final pickedFile = await ImagePicker().pickImage(source: ImageSource.gallery);
+  if (pickedFile != null) {
+    setState(() {
+      imagePath = pickedFile.path;
+      points.clear();
+      undonePoints.clear();
+    });
+  }
+}
+}
+
